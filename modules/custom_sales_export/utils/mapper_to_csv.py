607f7e3a298cf47a7ccfc7f317a2b4084d916275
David Gonzalez <dav6wat@gmail.com>
2026-02-18 12:36:19 +0100

    Return correct fields in csv

--- modules/custom_sales_export/utils/mapper_to_csv.py
+++ modules/custom_sales_export/utils/mapper_to_csv.py
@@ -8,32 +8,37 @@ class MapperToCsv:
 
 
     def _get_client_code(self, company_id):
-        count_config = self.config_data.counts.get(company_id)
-        # If store_id don't exist in excel return base account
-        if not count_config:
-            return "430000000"
-
-        if count_config.CON_ClientsUnic == 'S':
+        # If store_id(company_id) don't exist I use store 9 because seems to be generic store
+        count_config = self.config_data.counts.get(company_id) or self.config_data.counts.get(9)
+        
+        # If store_id exist in excel return CON_ClientsUnicCuenta
+        if count_config.CON_ClientsUnic.upper() == 'S':
             return count_config.CON_ClientsUnicCuenta
 
         # If isn't unic account, return base account because i don't have privileges to create accounts
-        return "430000000"
+        return self.config_data.counts.get(9).CON_ClientsUnicCuenta
 
 
     def _get_product_family(self, company_id, product_family):
-        # If store_id don't exist i use generic store (9)
+        # If store_id(company_id) don't exist I use store 9 because seems to be generic store
         count_config = self.config_data.counts.get(company_id) or self.config_data.counts.get(9)
-
-        if count_config.CON_FamUnic == 'S':
-            return count_config.CON_FamUnicCuenta
-
-        # If store_id don't exist i use generic store (9)
         family_config = self.config_data.counts_values.get(company_id) or self.config_data.counts_values.get(9)
-        # If product_family don't exist i use generic product id (14) because 
-        # name (OTROS) is an inconsistent field (OTROS, OTROS ALCALDE ,...)
+        # If product_family don't exist i use product_id 14 because seems to be generic product
+        # I use product_id because name is an inconsistent field (OTROS, OTROS ALCALDE ,...)
         family_line = family_config.get(product_family) or family_config.get(14)
 
-        return family_line.CONV_Mask
+        # If store_id exist in excel return CON_FamUnicCuenta and family label
+        if count_config.CON_FamUnic.upper() == 'S':
+            return {
+                'family_line': family_line.CONV_Familia,
+                'revenue_account_code': count_config.CON_FamUnicCuenta
+            }
+
+        # If CON_FamUnic == 'N', I return CONV_Mask and family label
+        return {
+            'family_line': family_line.CONV_Familia,
+            'revenue_account_code': family_line.CONV_Mask
+        }
 
 
     def _create_sales_order_csv(self):
@@ -64,14 +69,16 @@ class MapperToCsv:
                 row = {}
 
                 row['order_external_id'] = sale.name
-                row['revenue_account_code'] = self._get_client_code(sale.company_id.id)
-                row['qty'] = line.product_uom_qty
                 row['line_id'] = line.id
                 row['product_code'] = line.product_id.id
                 row['product_name'] = line.product_id.name
                 row['product_family'] = self._get_product_family(
                     sale.company_id.id, line.product_id.categ_id.id
-                )
+                )['family_line']
+                row['revenue_account_code'] = self._get_product_family(
+                    sale.company_id.id, line.product_id.categ_id.id
+                )['revenue_account_code']
+                row['qty'] = line.product_uom_qty
                 row['unit_price'] = line.price_unit
                 row['discount'] = line.discount
                 row['subtotal'] = line.price_subtotal
